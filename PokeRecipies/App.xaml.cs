c1936650fe475db9b38826ee55babc5b2b848cf6
Angel Garza <agarza@aph.mx>
2025-08-21 16:29:06 -0600

    initial commit

---
+++ PokeRecipies/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PokeRecipies
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PokeRecipies/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PokeRecipies
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
